8fa136267f83653776ebf52962d31160cfd01696
Yarraguntlasushmitha <160024915+Yarraguntlasushmitha@users.noreply.github.com>
2024-03-11 16:01:05 +0530

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,54 @@
+'''
+controls and data structures->ABC language
+Strings and its formats->from C
+Regular expressions->Pearl
+Exception handling->Module 3
+Magic methods from->fortron
+Python is a interpreted,compiled,Dynamically typed languages
+Cpython-Written in C, most common implementation of python.
+'''
+
+
+
+
+
+
+#print("hello")
+#print("hello 1")
+'''
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+'''
+#print("hello world")
+#import this
+#print(this)
+#import antigravity
+#print(object,sep,end,file,flush)
+#print has 5 parameters
+#object -->the value to be printed is object
+#sep-->seperator
+#eg
+#a=8
+#b=9
+#c=89
+#print(a,b,c, sep='$')
+#end-->which is used to add the string at the end of the value,
+##It will not keep the command to stay in same line
+#eg:
+#print(str(a),b,end='%%')
+#print(str(c))
+#!---> variables
+# ?variable is a name which is used to identify the value of variable
+# ? which is stored in a specific memory location
+# !Rules to dedfine variables
+#1.)variable have to start with only alphabets, underscores
+#2.)The char following the 1st char can be alphabets,numbers,underscores
+#3.)No spaces are allowed
+#4.)No special characters like !@#$%^&*()+
+#5.)case sensitive
+#6.)keywords are not allowed as variables
+a,b=78
+print(a,b)
